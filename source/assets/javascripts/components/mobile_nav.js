66c9fe4805e9e6ef448549bd534b6bd2123249a4
florian Dublanc <floriandbc@gmail.com>
2020-02-12 13:01:22 +0100

    hambuerger encore

--- source/assets/javascripts/components/mobile_nav.js
+++ source/assets/javascripts/components/mobile_nav.js
@@ -1,25 +1,19 @@
-// eslint-disable-next-line no-use-before-define
-newFunction();
+// $(document).ready(function() {
+//   $('#nav-icon').click(function() {
+//     $(this).toggleClass('open');
+//   });
+// });
 
-function newFunction() {
-  $(document).ready(function() {
-    $('#nav-icon').click(function() {
-      $(this).toggleClass('open');
-    });
-  });
+const sidenavInner = document.querySelector('.sidenav');
+const { body } = document;
+
+function toggleNav() {
+  sidenavInner.classList.toggle('show-sidenav');
+  if (sidenavInner.classList.contains('show-sidenav')) {
+    body.classList.add('freezePage');
+  } else {
+    body.classList.remove('freezePage');
+  }
 }
-// const sidenavInner = document.querySelector('.sidenav');
-// const { body } = document;
 
-// function toggleNav() {
-//   sidenavInner.classList.toggle('show-sidenav');
-//   if (sidenavInner.classList.contains('show-sidenav')) {
-//     body.classList.add('freezePage');
-//   } else {
-//     body.classList.remove('freezePage');
-//   }
-// }
-
-// export { toggleNav };
-
-// const hamburgerEffect = document.querySelector('')
+export { toggleNav };
